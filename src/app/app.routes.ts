f79c8cf2a5afe4240890915b716129cdc33811f0
Ellen <lidenellen@gmail.com>
2025-08-04 15:41:11 +0200

    lagt till sidorna ramschema (schedule) och kurser (courses) med fungerande menylänkar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Contact } from './contact/contact';
+import { Courses } from './courses/courses';
+import { Schedule } from './schedule/schedule';
 
 export const routes: Routes = [
   { path: 'contact', component: Contact },
+  { path: 'courses', component: Courses },
+  { path: 'schedule', component: Schedule },
   { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
