0b2baf7c01c791b744945e66920f468af62061b6
Ellen <lidenellen@gmail.com>
2025-08-05 12:57:55 +0200

    lagt till en startsida och stylat den, hemknapp i navbar.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Contact } from './contact/contact';
 import { Courses } from './courses/courses';
 import { Schedule } from './schedule/schedule';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'contact', component: Contact },
   { path: 'courses', component: Courses },
   { path: 'schedule', component: Schedule },
-  { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
